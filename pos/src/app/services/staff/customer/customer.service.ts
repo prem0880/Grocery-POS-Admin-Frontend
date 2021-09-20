151f9a248a1ca2a5be200a0bb8d6ab9a5eec508d
Kirithiha <kirithiha2000@gmail.com>
2021-09-20 23:03:13 +0530

    done with service

--- pos/src/app/services/staff/customer/customer.service.ts
+++ pos/src/app/services/staff/customer/customer.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { StringifyOptions } from 'querystring';
 import { Observable } from 'rxjs';
 
 @Injectable({
